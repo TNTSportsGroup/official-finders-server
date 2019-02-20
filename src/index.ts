47f411b93fb5f181019d33b677a4e4186bd6684a
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-20 14:29:24 -0600

    made use of the express-fileupload

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { hwrpRouter } from "./routers/hwrp";
+import fileUpload from "express-fileupload";
 
 const port = 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 }
+  })
+);
 
 app.use("/hwrp", hwrpRouter);
 
