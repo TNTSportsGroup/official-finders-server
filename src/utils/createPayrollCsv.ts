8c27662aae6a1e9f39755ff49b4e5aeda8b5fbe5
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-20 16:41:48 -0600

    updated our createPayrollCsv

--- src/utils/createPayrollCsv.ts
+++ src/utils/createPayrollCsv.ts
@@ -1,22 +1,22 @@
-import createCsvWriter from 'csv-writer'
-import { create } from 'domain';
-
+import createCsvWriter from "csv-writer";
+import * as shortid from "shortid";
 
 interface Header {
-    id: string;
-    title: string;
+  id: string;
+  title: string;
 }
 
-export const createPayrollCsv = (fileName: string, headers: Header[], records: any) => {
+export const createPayrollCsv = (
+  fileName: string,
+  headers: Header[],
+  records: any
+) => {
+  const csvWriter = createCsvWriter({
+    path: `csvs/${fileName}.csv`,
+    headers: headers
+  });
 
-    const csvWriter = createCsvWriter({
-        path: `csvs/${fileName}.csv`,
-        headers: headers
-    })
-
-
-    csvWriter.writeRecords(records)
-        .then(() => {
-            console.log('...Done')
-        })
-}
+  csvWriter.writeRecords(records).then(() => {
+    console.log("...Done");
+  });
+};
