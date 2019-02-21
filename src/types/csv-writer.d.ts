c0964d647b1595cb0795a0c123bda051b6c8436d
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-21 12:05:57 -0600

    created a type defs for csv-writer

---
+++ src/types/csv-writer.d.ts
@@ -0,0 +1,18 @@
+// declare function createCsvWriter({
+//   path,
+//   headers
+// }: {
+//   path: string;
+//   headers: any[];
+// }): csvWriter;
+
+// declare type csvWriter = {
+//   writeRecords: (records: any) => any;
+// };
+
+// declare module "csv-writer" {
+//   //   export = createCsvWriter;
+//   export function createObjectCsvWriter(): createCsvWriter;
+// }
+
+declare module "csv-writer";
