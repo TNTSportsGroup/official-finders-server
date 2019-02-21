f84f5083f386c8f8d4b5a3f9c250d85d2f141c30
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-21 12:06:46 -0600

    made some big changes to our createPayrollCsv updated the path using path.resolve and __dirname and fixed some typos

--- src/utils/createPayrollCsv.ts
+++ src/utils/createPayrollCsv.ts
@@ -1,5 +1,6 @@
-import createCsvWriter from "csv-writer";
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 import * as shortid from "shortid";
+import path from "path";
 
 interface Header {
   id: string;
@@ -10,13 +11,18 @@ export const createPayrollCsv = (headers: Header[], records: any) => {
   const name = `${shortid.generate()}.csv`;
 
   const csvWriter = createCsvWriter({
-    path: `csvs/${name}`,
-    headers: headers
+    path: path.resolve(__dirname + `/../csvs/${name}`),
+    header: headers
   });
 
-  csvWriter.writeRecords(records).then(() => {
-    console.log("...Done");
+  csvWriter
+    .writeRecords(records)
+    .then(() => {
+      console.log("...Done");
+    })
+    .catch(e => {
+      console.log(e);
+    });
 
-    return name;
-  });
+  return name;
 };
