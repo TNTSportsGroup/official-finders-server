66a5a86f91b5f1389a2b90a8f90197e95db71463
Camilo Guerrero <camiloguerrero@Phils-Mac-mini.local>
2019-02-21 12:07:31 -0600

    updated our post method we use of createPayrollcsv and created headers for our csv file

--- src/routers/hwrp.ts
+++ src/routers/hwrp.ts
@@ -1,16 +1,30 @@
 import express from "express";
 import { scrapeHWRP } from "../utils/scrapeHwrp";
+import { createPayrollCsv } from "../utils/createPayrollCsv";
 
 export const hwrpRouter = express.Router();
 
 hwrpRouter.post("/", (req, res) => {
   if (req.files && req.files.file) {
     let file: any = req.files.file;
+    console.log(file);
     const { data } = file;
     let html = data.toString();
 
     const userData = scrapeHWRP(html);
-    console.log(userData);
+    const headers = [
+      {
+        id: "name",
+        title: "Name"
+      },
+      {
+        id: "payment",
+        title: "Payment"
+      }
+    ];
+
+    const name = createPayrollCsv(headers, userData);
+    console.log(name);
   }
 
   res.send("hi");
