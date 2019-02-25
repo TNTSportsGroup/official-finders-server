9ab7db7100b7127b777f4e2ebae0a4cfbecbce9f
datdevboi <cami172013@gmail.com>
2019-02-25 14:51:46 -0600

    created a sorting function that will sort by first name

---
+++ src/utils/sortByFirstName.ts
@@ -0,0 +1,18 @@
+import { User } from "./scrapeHwrp";
+
+export const sortByFirstName = (data: User[]) => {
+  return data.sort((a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    return 0;
+  });
+};
