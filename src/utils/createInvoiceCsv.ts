e72469a6b9ba7bda4bdaddc3bf38687bbd9d2bd5
datdevboi <cami172013@gmail.com>
2019-02-26 16:35:35 -0600

    created the function createInvoiceCsv that will create csvs

---
+++ src/utils/createInvoiceCsv.ts
@@ -0,0 +1,33 @@
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+import * as shortid from "shortid";
+import fs from "fs";
+import path from "path";
+import { createPayrollCsv, Header } from "./createPayrollCsv";
+import { IGameInvoice } from "./scrape/scrapeHwrInvoices";
+
+export const createInvoiceCsvs = (headers: Header[], records) => {
+  const folderName = shortid.generate();
+  fs.mkdirSync(path.resolve(__dirname + `/../csvs/invoices/${folderName}`));
+
+  const keys = Object.keys(records);
+
+  for (const key of keys) {
+    const csvWriter = createCsvWriter({
+      path: path.resolve(
+        __dirname + `/../csvs/invoices/${folderName}/${key}.csv`
+      ),
+      header: headers
+    });
+
+    csvWriter
+      .writeRecords(records[key].games)
+      .then(() => {
+        console.log("...Done");
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  return folderName;
+};
