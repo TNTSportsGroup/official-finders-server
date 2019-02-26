14eb3bbe87d8f02ecfd11a445b9970b9ea05ffce
datdevboi <cami172013@gmail.com>
2019-02-26 13:56:57 -0600

    added anew test case for single parethesis and made a new test case for double parenthesis.

--- src/__tests__/getInvoiceName.test.ts
+++ src/__tests__/getInvoiceName.test.ts
@@ -1,8 +1,16 @@
 import { getInvoiceName } from "../utils/getInvoiceName";
 
 describe("getInvoiceName", () => {
-  test("single parenthese", () => {
+  test("single parenthesis", () => {
     const str = "(Carol Stream PD - Dodgeball) 6th Grade In-House";
+    const str2 = "(Streamwood PD - Basketball) 8th Grade Boys In-House";
     expect(getInvoiceName(str)).toBe("Carol Stream PD - Dodgeball");
+    expect(getInvoiceName(str2)).toBe("Streamwood PD - Basketball");
+  });
+
+  test("double parenthesis", () => {
+    const str = "(YMCA Downers Grove (IBY) - Basketball)";
+
+    expect(getInvoiceName(str)).toBe("YMCA Downers Grove IBY - Basketball");
   });
 });
