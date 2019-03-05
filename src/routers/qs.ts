f8617021033614c8a057dc9ad761b07c612af5c4
datdevboi <cami172013@gmail.com>
2019-03-05 14:15:29 -0600

    updated our route

--- src/routers/qs.ts
+++ src/routers/qs.ts
@@ -13,17 +13,34 @@ QsRouter.get("/", async (req, res) => {
     QUICKSCOREDIR.GLEN_ELLYN_PARK_DISTRICT,
     process.env.GLEN_ELLYN_PARK_DISTRICT
   );
-
+  // Get event list
   const data = await demo.eventList();
+  // filter by the season
   const seasonSchedule = data.filter(item => item.Season === "Winter 2019");
-  const firstLeague = seasonSchedule[0];
+  let upcomingGames = {};
 
-  let gamesForLeague = await demo.scheduleInfo(firstLeague.LeagueID);
-  const gameData = gamesForLeague.RegularGameData.filter(
-    game => game.Date >= "2019-03-01"
-  );
+  for (let league of seasonSchedule) {
+    upcomingGames[league.LeagueID] = [];
+
+    let { RegularGameData, LeagueName } = await demo.scheduleInfo(
+      league.LeagueID
+    );
+    let newData = RegularGameData.filter(game => game.Date >= "2019-03-04");
+
+    newData.forEach(game => {
+      upcomingGames[league.LeagueID].push({
+        GameID: game.GameID,
+        LeagueName,
+        Date: game.Date,
+        Time: game.Time,
+        LocationName: game.LocationName,
+        TeamName1: game.TeamName1,
+        TeamName2: game.TeamName2
+      });
+    });
+  }
 
   res.send({
-    data: firstLeague
+    data: upcomingGames
   });
 });
