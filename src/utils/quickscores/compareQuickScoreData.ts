47b6b7562964bed28e89eae9c0bad677588ff186
datdevboi <cami172013@gmail.com>
2019-03-08 12:12:14 -0600

    updated our compareQuickScoreData function we returned an object with the differentRecords array and a newGames array.

--- src/utils/quickscores/compareQuickScoreData.ts
+++ src/utils/quickscores/compareQuickScoreData.ts
@@ -2,25 +2,26 @@ import { isEqual } from "lodash";
 
 export const compareQuickScoreData = (previousData: any, currentData: any) => {
   let differentRecords = [];
+  let newGames = [];
 
   const currentDataLeagues = Object.keys(currentData);
 
   for (let league of currentDataLeagues) {
-    if (previousData[league]) {
-      currentData[league].forEach(game => {
-        let [oldGameData] = previousData[league].filter(
-          previousGame => previousGame.GameID === game.GameID
-        );
+    currentData[league].forEach(game => {
+      let [oldGameData] = previousData[league].filter(
+        previousGame => previousGame.GameID === game.GameID
+      );
 
-        if (!isEqual(game, oldGameData)) {
-          differentRecords.push({
-            oldData: oldGameData,
-            new: game
-          });
-        }
-      });
-    }
+      if (!oldGameData) {
+        newGames.push(game);
+      } else if (!isEqual(game, oldGameData)) {
+        differentRecords.push({
+          oldData: oldGameData,
+          new: game
+        });
+      }
+    });
   }
 
-  return differentRecords;
+  return { differentRecords, newGames };
 };
