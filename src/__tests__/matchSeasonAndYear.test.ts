c4c4069b8541645bb0c36b7d19cfa982d8bc2ac2
datdevboi <cami172013@gmail.com>
2019-03-11 15:16:54 -0500

    set up some test for our matchSeasonAndYear

---
+++ src/__tests__/matchSeasonAndYear.test.ts
@@ -0,0 +1,21 @@
+import { matchSeasonAndYear } from "../utils/quickscores/matchSeasonAndYear";
+
+describe("matchSeasonAndYear", () => {
+  test("Match Spring with year variations", () => {
+    const matchSpring = matchSeasonAndYear("Spring", 2019);
+
+    expect(matchSpring("Spring 2019")).toBe("Spring 2019");
+    expect(matchSpring("Spring 2018-2019")).toBe("Spring 2018-2019");
+    expect(matchSpring("Spring 2019-2020")).toBe("Spring 2019-2020");
+  });
+
+  test("Match Winter with year variations", () => {
+    const matchSpring = matchSeasonAndYear("Winter", 2019);
+
+    expect(matchSpring("Winter 2019")).toBe("Winter 2019");
+    expect(matchSpring("Winter 2018-2019")).toBe("Winter 2018-2019");
+    expect(matchSpring("Winter 2019-2020")).toBe("Winter 2019-2020");
+  });
+
+  //   test("checks positives", () => {});
+});
