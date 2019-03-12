4d349e7c6c7e4c5f097319f21957ab823f7c03b2
datdevboi <cami172013@gmail.com>
2019-03-12 13:23:55 -0500

    added comments for better readability.

--- src/utils/makeCsvDirectories.ts
+++ src/utils/makeCsvDirectories.ts
@@ -2,8 +2,11 @@ import fs from "fs";
 import path from "path";
 
 export const makeCsvDirectories = async () => {
+  // check if the csvs folder exist
   fs.access(path.resolve(__dirname + "/../csvs"), err => {
     if (err) {
+      // if the csvs folder doens't exist we create the parent folder
+      // payroll, invoices, and quickscores
       fs.mkdirSync(path.resolve(__dirname + "/../csvs"));
       fs.mkdirSync(path.resolve(__dirname + "/../csvs/payroll"));
       fs.mkdirSync(path.resolve(__dirname + "/../csvs/invoices"));
