f96e0a5fcf7b2e8b30d50ff82b6dd473a60f0fd4
datdevboi <cami172013@gmail.com>
2019-03-12 13:10:38 -0500

    put the correct directory names in our mkdirSync functions.

--- src/utils/makeCsvDirectories.ts
+++ src/utils/makeCsvDirectories.ts
@@ -2,13 +2,12 @@ import fs from "fs";
 import path from "path";
 
 export const makeCsvDirectories = async () => {
-  fs.access(path.resolve(__dirname + "/../nani"), err => {
-    console.log(err);
+  fs.access(path.resolve(__dirname + "/../csvs"), err => {
     if (err) {
-      fs.mkdirSync(path.resolve(__dirname + "/../nani"));
-      fs.mkdirSync(path.resolve(__dirname + "/../nani/payroll"));
-      fs.mkdirSync(path.resolve(__dirname + "/../nani/invoices"));
-      fs.mkdirSync(path.resolve(__dirname + "/../nani/quickscores"));
+      fs.mkdirSync(path.resolve(__dirname + "/../csvs"));
+      fs.mkdirSync(path.resolve(__dirname + "/../csvs/payroll"));
+      fs.mkdirSync(path.resolve(__dirname + "/../csvs/invoices"));
+      fs.mkdirSync(path.resolve(__dirname + "/../csvs/quickscores"));
     }
   });
 };
