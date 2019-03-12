06c3a1d18b9e3ded1821491209a7446dd0ae0552
datdevboi <cami172013@gmail.com>
2019-03-12 13:33:19 -0500

    added comments.

--- src/utils/isNegative.ts
+++ src/utils/isNegative.ts
@@ -1,4 +1,6 @@
 export const isNegative = (amount: string) => {
+  // check if strings starts with $-
+  // this will indicate that it's a negative number.
   const regex = /^\$-/;
 
   const result = amount.match(regex);
