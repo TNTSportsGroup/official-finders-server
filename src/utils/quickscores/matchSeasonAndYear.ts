8a4c15662499370ee98b779df40eeee41950913c
datdevboi <cami172013@gmail.com>
2019-03-13 12:19:24 -0500

    updated our matchSeasonAndYear function we checked if there weren't any matches, if no then return false otherwise return true;

--- src/utils/quickscores/matchSeasonAndYear.ts
+++ src/utils/quickscores/matchSeasonAndYear.ts
@@ -1,11 +1,19 @@
 export const matchSeasonAndYear = (season: string, year: number) => (
   str: string
-) => {
+): boolean => {
   let regxp = `${season}\\s(${year - 1}-${year}|${year}-${year + 1}|${year})`;
 
   const seasonRegularExpression = new RegExp(regxp, "g");
 
-  const [firstMatch] = str.match(seasonRegularExpression);
+  const matches = str.match(seasonRegularExpression);
+  console.log(matches);
 
-  return firstMatch;
+  if (!matches) {
+    return false;
+  }
+
+  if (matches.length > 1) {
+    return true;
+  }
+  return false;
 };
