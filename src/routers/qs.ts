74cd73cc930fd81a1ecedb554f04680e43d06806
datdevboi <cami172013@gmail.com>
2019-03-13 14:47:36 -0500

    updated our interface imports and made use of our getUpcomingGames function.

--- src/routers/qs.ts
+++ src/routers/qs.ts
@@ -8,77 +8,12 @@ import {
 } from "../utils/quickscores/createGamesCsv";
 import { matchSeasonAndYear } from "../utils/quickscores/matchSeasonAndYear";
 import { writeObjWithRedis } from "../utils/quickscores/usingRedis";
-
-export interface IGame {
-  GameID: string;
-  LeagueName: string;
-  Date: string;
-  Time: string;
-  LocationName: string;
-  HomeTeam: string;
-  AwayTeam: string;
-}
-
-export interface ILeagueTable {
-  [key: string]: IGame[];
-}
-
-const QUICKSCOREDIR = {
-  GLEN_ELLYN_PARK_DISTRICT: "glenellyn",
-  GLEN_ELLYN_YOUTH_BASEBALL: "geyba",
-  GENEVA_PARK_DISTRICT: "genevaparks",
-  GIRLS: "girls",
-  WHEATON_PARK_DISTRICT: "wheaton",
-  CAROL_STREAM_PARK_DISTRICT: "csparks"
-};
-
-const seasonToFilterBy = matchSeasonAndYear("Winter", 2019);
-
-const filterByLeagueSeason = filterBy<IEvent>(league =>
-  seasonToFilterBy(league.Season)
-);
-
-const filterByDate = filterBy<IGameData>(game => game.Date >= "2019-03-13");
+import { getUpcomingGames } from "../utils/quickscores/getUpcomingGames";
 
 export const QsRouter = express.Router();
 
 QsRouter.get("/", async (req, res) => {
-  const demo = new QuickScoreReq(
-    QUICKSCOREDIR.GLEN_ELLYN_PARK_DISTRICT,
-    process.env.GLEN_ELLYN_PARK_DISTRICT
-  );
-  // Get event list
-  const data = await demo.eventList();
-
-  // TODO if carol stream only get soccer volleyball hockey dodgeball
-
-  // filter by the season
-
-  const seasonSchedule = filterByLeagueSeason(data);
-
-  let upcomingGames: ILeagueTable = {};
-
-  for (let league of seasonSchedule) {
-    upcomingGames[league.LeagueID] = [];
-
-    let { RegularGameData, LeagueName } = await demo.scheduleInfo(
-      league.LeagueID
-    );
-
-    let newData = filterByDate(RegularGameData);
-
-    newData.forEach(game => {
-      upcomingGames[league.LeagueID].push({
-        GameID: game.GameID,
-        LeagueName,
-        Date: game.Date,
-        Time: game.Time,
-        LocationName: game.LocationName,
-        HomeTeam: game.TeamName1,
-        AwayTeam: game.TeamName2
-      });
-    });
-  }
+  const upcomingGames = await getUpcomingGames();
 
   const { newGames, updatedGames } = await compareQuickScoreData(
     "Winter 2019",
