74ffbbdef922776be82c1153ce6e0c02ddee11e0
datdevboi <cami172013@gmail.com>
2019-03-18 12:11:33 -0500

    updated our Redis constructor. We passed both the port and host  that we get form the keys object.

--- src/redis.ts
+++ src/redis.ts
@@ -1,4 +1,8 @@
 import Redis from "ioredis";
+import { keys } from "./keys";
 
 // made an instance of redis and exported it.
-export const redis = new Redis();
+export const redis = new Redis({
+  port: parseInt(keys.REDIS_PORT),
+  host: keys.REDIS_HOST
+});
