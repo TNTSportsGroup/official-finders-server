208eaa4b8475efa20a940719fb5890c380149b64
datdevboi <cami172013@gmail.com>
2019-03-20 15:48:57 -0500

    updated redis constructor

--- src/redis.ts
+++ src/redis.ts
@@ -2,7 +2,9 @@ import Redis from "ioredis";
 import { keys } from "./keys";
 
 // made an instance of redis and exported it.
+
+console.log(keys.REDIS_HOST);
 export const redis = new Redis({
-  port: parseInt(keys.REDIS_PORT),
+  port: parseInt(keys.REDIS_PORT) || 6379,
   host: keys.REDIS_HOST
 });
