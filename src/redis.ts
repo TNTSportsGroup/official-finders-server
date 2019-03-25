7d6b6c8996fcc32fb52f5e5e16d6598c2f962a89
datdevboi <cami172013@gmail.com>
2019-03-25 15:39:51 -0500

    modified our redis intance

--- src/redis.ts
+++ src/redis.ts
@@ -3,14 +3,20 @@ import { keys } from "./keys";
 import IORedisMock from 'ioredis-mock'
 
 // made an instance of redis and exported it.
+export let redis;
 
-export let redis = new IORedis({
-  port: parseInt(keys.REDIS_PORT) || 6379,
-  host: keys.REDIS_HOST || 'redis',
-  enableReadyCheck: true
-});
+
 
 if(process.env.NODE_ENV === "test") {
   redis = new IORedisMock();
+} else {
+  redis = new IORedis({
+    port: parseInt(keys.REDIS_PORT) || 6379,
+    host: keys.REDIS_HOST || 'redis',
+    enableReadyCheck: true
+  });
 }
+
+
+redis.Promise = global.Promise;
 
