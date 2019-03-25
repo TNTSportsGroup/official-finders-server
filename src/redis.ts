f0f9374c4f8492c929d483749bf38d652d0236e0
datdevboi <cami172013@gmail.com>
2019-03-25 14:36:14 -0500

    updated stuff

--- src/redis.ts
+++ src/redis.ts
@@ -3,8 +3,10 @@ import { keys } from "./keys";
 
 // made an instance of redis and exported it.
 
+console.log(keys.REDIS_PORT);
+console.log(keys.REDIS_HOST);
 export const redis = new Redis({
   port: parseInt(keys.REDIS_PORT) || 6379,
-  host: keys.REDIS_HOST || 'localhost',
+  host: keys.REDIS_HOST || 'redis',
   enableReadyCheck: true
 });
