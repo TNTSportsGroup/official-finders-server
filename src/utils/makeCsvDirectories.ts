cc645105892351886724132a5549173be3dbd628
datdevboi <cami172013@gmail.com>
2019-03-26 11:23:55 -0500

    Made our functions more reusable
    created makeCsvDirectory function that will check if a directory exist and if not
    it will create it. We then used this function in our
    makeCsvDirectories.
    This makes the code more readable.

--- src/utils/makeCsvDirectories.ts
+++ src/utils/makeCsvDirectories.ts
@@ -1,16 +1,23 @@
 import fs from "fs";
 import path from "path";
 
+const fsPromises = fs.promises;
+
 export const makeCsvDirectories = async () => {
-  // check if the csvs folder exist
-  fs.access(path.resolve(__dirname + "/../csvs"), err => {
-    if (err) {
-      // if the csvs folder doens't exist we create the parent folder
-      // payroll, invoices, and quickscores
-      fs.mkdirSync(path.resolve(__dirname + "/../csvs"));
-      fs.mkdirSync(path.resolve(__dirname + "/../csvs/payroll"));
-      fs.mkdirSync(path.resolve(__dirname + "/../csvs/invoices"));
-      fs.mkdirSync(path.resolve(__dirname + "/../csvs/quickscores"));
-    }
-  });
+  await makeCsvDirectory(path.resolve(__dirname + "/../csvs/payroll"));
+  await makeCsvDirectory(path.resolve(__dirname + "/../csvs/invoices"));
+  await makeCsvDirectory(path.resolve(__dirname + "/../csvs/quickscores"));
+};
+
+const makeCsvDirectory = async (path: string) => {
+  try {
+    // we check if the directory exist
+    await fsPromises.access(path);
+  } catch (e) {
+    // if there was an error such as the directory
+    // no existing then we create the directory.
+    await fsPromises.mkdir(path, {
+      recursive: true
+    });
+  }
 };
