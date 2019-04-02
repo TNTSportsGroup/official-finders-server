d98e905b2f1cf2caf4c5c4099c468febc7771f54
datdevboi <cami172013@gmail.com>
2019-04-02 14:47:00 -0500

    Added more test

--- src/__tests__/matchSport.test.ts
+++ src/__tests__/matchSport.test.ts
@@ -4,5 +4,10 @@ describe("matchSport", () => {
   test("test matchSport", () => {
     expect(matchSport("Basketball", "Adult Basketball")).toBe(true);
     expect(matchSport("Basketball", "Adult Baseball")).toBe(false);
+
+    expect(matchSport("Soccer", "Adult Soccer")).toBe(true);
+    expect(matchSport("Soccer", "Adult Baseball")).toBe(false);
+    expect(matchSport("Soccer", "Youth Soccer")).toBe(true);
+    expect(matchSport("Soccer", "Adult Volleyball")).toBe(false);
   });
 });
