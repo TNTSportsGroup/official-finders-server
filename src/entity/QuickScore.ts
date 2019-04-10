49843dcdd46a438b624f9eee501159b30b2f9c00
Camilo Guerrero <cami172013@gmail.com>
2019-04-10 13:29:54 -0500

    Created a  QuickScore entity and removed our old User entity

---
+++ src/entity/QuickScore.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@Entity("quickscores")
+export class QuickScore extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid") id: string;
+
+  @Column("varchar", { length: 255, nullable: false })
+  season: string;
+
+  @Column("int", { nullable: false })
+  year: number;
+
+  @Column("json")
+  data: string;
+}
