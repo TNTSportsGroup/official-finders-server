6fd598e225171d7a7ef8e75a22bf1491a170cbbf
Camilo Guerrero <cami172013@gmail.com>
2019-04-10 21:46:41 -0500

    updated the createConnection options

--- src/createDatabaseConn.ts
+++ src/createDatabaseConn.ts
@@ -3,5 +3,5 @@ import { createConnection, getConnectionOptions } from "typeorm";
 export const createDatabaseConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
 
-  return createConnection({ ...connectionOptions });
+  return createConnection({ ...connectionOptions, name: process.env.NODE_ENV });
 };
