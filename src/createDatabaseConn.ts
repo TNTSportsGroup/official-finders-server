2a0ca4a31b68adff9ff8b4582387136df7e3a62b
Camilo Guerrero <cami172013@gmail.com>
2019-04-12 13:00:51 -0500

    removed console log

--- src/createDatabaseConn.ts
+++ src/createDatabaseConn.ts
@@ -3,8 +3,6 @@ import { createConnection, getConnectionOptions } from "typeorm";
 export const createDatabaseConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
 
-  console.log(connectionOptions);
-
   return createConnection({
     ...connectionOptions,
     name: "default"
