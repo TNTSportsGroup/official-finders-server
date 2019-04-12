c529d032fafcd524763c3838bfc2924339657987
Camilo Guerrero <cami172013@gmail.com>
2019-04-12 13:00:00 -0500

    updated the connection name.

--- src/createDatabaseConn.ts
+++ src/createDatabaseConn.ts
@@ -3,5 +3,10 @@ import { createConnection, getConnectionOptions } from "typeorm";
 export const createDatabaseConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
 
-  return createConnection({ ...connectionOptions, name: process.env.NODE_ENV });
+  console.log(connectionOptions);
+
+  return createConnection({
+    ...connectionOptions,
+    name: "default"
+  });
 };
